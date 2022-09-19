53654b945a2c14f660785a97416d4cb1045b7cd9
cristianrieth <cristiangrieth@gmail.com>
2022-09-19 13:10:20 -0300

    Registration of people using SQLite - C#
    Registration of people using SQLite - C#

---
+++ CadastroAlunos/CadastroAlunos/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace CadastroAlunos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Id = new System.Windows.Forms.Label();
+            this.CEP = new System.Windows.Forms.Label();
+            this.UF = new System.Windows.Forms.Label();
+            this.Cidade = new System.Windows.Forms.Label();
+            this.Endereco = new System.Windows.Forms.Label();
+            this.Sobrenome = new System.Windows.Forms.Label();
+            this.Nome = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.cbxUF = new System.Windows.Forms.ComboBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.dgwDados = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Id
+            // 
+            this.Id.AutoSize = true;
+            this.Id.Location = new System.Drawing.Point(13, 23);
+            this.Id.Name = "Id";
+            this.Id.Size = new System.Drawing.Size(16, 13);
+            this.Id.TabIndex = 0;
+            this.Id.Text = "Id";
+            this.Id.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CEP
+            // 
+            this.CEP.AutoSize = true;
+            this.CEP.Location = new System.Drawing.Point(13, 203);
+            this.CEP.Name = "CEP";
+            this.CEP.Size = new System.Drawing.Size(28, 13);
+            this.CEP.TabIndex = 1;
+            this.CEP.Text = "CEP";
+            // 
+            // UF
+            // 
+            this.UF.AutoSize = true;
+            this.UF.Location = new System.Drawing.Point(13, 173);
+            this.UF.Name = "UF";
+            this.UF.Size = new System.Drawing.Size(21, 13);
+            this.UF.TabIndex = 2;
+            this.UF.Text = "UF";
+            // 
+            // Cidade
+            // 
+            this.Cidade.AutoSize = true;
+            this.Cidade.Location = new System.Drawing.Point(13, 143);
+            this.Cidade.Name = "Cidade";
+            this.Cidade.Size = new System.Drawing.Size(40, 13);
+            this.Cidade.TabIndex = 3;
+            this.Cidade.Text = "Cidade";
+            // 
+            // Endereco
+            // 
+            this.Endereco.AutoSize = true;
+            this.Endereco.Location = new System.Drawing.Point(13, 113);
+            this.Endereco.Name = "Endereco";
+            this.Endereco.Size = new System.Drawing.Size(53, 13);
+            this.Endereco.TabIndex = 4;
+            this.Endereco.Text = "Endereço";
+            // 
+            // Sobrenome
+            // 
+            this.Sobrenome.AutoSize = true;
+            this.Sobrenome.Location = new System.Drawing.Point(13, 83);
+            this.Sobrenome.Name = "Sobrenome";
+            this.Sobrenome.Size = new System.Drawing.Size(61, 13);
+            this.Sobrenome.TabIndex = 5;
+            this.Sobrenome.Text = "Sobrenome";
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(13, 53);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(35, 13);
+            this.Nome.TabIndex = 6;
+            this.Nome.Text = "Nome";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(133, 20);
+            this.txtId.Name = "txtId";
+            this.txtId.ReadOnly = true;
+            this.txtId.Size = new System.Drawing.Size(69, 20);
+            this.txtId.TabIndex = 7;
+            // 
+            // cbxUF
+            // 
+            this.cbxUF.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxUF.FormattingEnabled = true;
+            this.cbxUF.Items.AddRange(new object[] {
+            "AC",
+            "AL\t",
+            "AM",
+            "AP\t",
+            "BA\t",
+            "CE\t",
+            "DF\t",
+            "ES\t",
+            "GO\t",
+            "MA\t",
+            "MG\t",
+            "MS\t",
+            "MT\t",
+            "PA\t",
+            "PB\t",
+            "PE\t",
+            "PI\t",
+            "PR\t",
+            "RJ\t",
+            "RN\t",
+            "RO\t",
+            "RR\t",
+            "RS\t",
+            "SC\t",
+            "SE\t",
+            "SP",
+            "TO"});
+            this.cbxUF.Location = new System.Drawing.Point(133, 167);
+            this.cbxUF.Name = "cbxUF";
+            this.cbxUF.Size = new System.Drawing.Size(69, 21);
+            this.cbxUF.TabIndex = 8;
+            this.cbxUF.SelectedIndexChanged += new System.EventHandler(this.cbxUF_SelectedIndexChanged);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(133, 49);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 9;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(133, 79);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(121, 20);
+            this.txtSobrenome.TabIndex = 10;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(133, 115);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(158, 20);
+            this.txtEndereco.TabIndex = 11;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(133, 142);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(197, 20);
+            this.txtCidade.TabIndex = 12;
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(133, 202);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(100, 20);
+            this.txtCEP.TabIndex = 13;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(504, 23);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(75, 23);
+            this.btnNovo.TabIndex = 14;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(504, 60);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 15;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(504, 94);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 16;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(504, 129);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 17;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // dgwDados
+            // 
+            this.dgwDados.AllowUserToAddRows = false;
+            this.dgwDados.AllowUserToDeleteRows = false;
+            this.dgwDados.AllowUserToResizeColumns = false;
+            this.dgwDados.AllowUserToResizeRows = false;
+            this.dgwDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwDados.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgwDados.Location = new System.Drawing.Point(12, 240);
+            this.dgwDados.MultiSelect = false;
+            this.dgwDados.Name = "dgwDados";
+            this.dgwDados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgwDados.Size = new System.Drawing.Size(694, 169);
+            this.dgwDados.TabIndex = 18;
+            this.dgwDados.VirtualMode = true;
+            this.dgwDados.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgwDados_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgwDados);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btnNovo);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.cbxUF);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.Sobrenome);
+            this.Controls.Add(this.Endereco);
+            this.Controls.Add(this.Cidade);
+            this.Controls.Add(this.UF);
+            this.Controls.Add(this.CEP);
+            this.Controls.Add(this.Id);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwDados)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Id;
+        private System.Windows.Forms.Label CEP;
+        private System.Windows.Forms.Label UF;
+        private System.Windows.Forms.Label Cidade;
+        private System.Windows.Forms.Label Endereco;
+        private System.Windows.Forms.Label Sobrenome;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.ComboBox cbxUF;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.DataGridView dgwDados;
+    }
+}
+
---
+++ CadastroAlunos/CadastroAlunos/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Adicionar bibliotecas do SQLite
+using System.Data.SQLite;
+using System.Data.SqlClient;
+//Bibliotecas para texto
+
+
+
+namespace CadastroAlunos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Função para retornar uma conexão ao banco de dados
+        //Utilizar sempre que desejar executar um comando no banco de dados
+        private static SQLiteConnection ConexaoDB()
+        {
+            //Criar uma conexão ao banco
+            SQLiteConnection Conexao;
+            Conexao = new SQLiteConnection("Data Source=Alunos.db");
+            Conexao.Open();
+            return Conexao;
+        }
+        //Procedimento para atualizar a Grade de dados
+        //Utilizar sempre que quiser apresentar a situação atual dos dados
+        private void AtualizaGrid()
+        {
+            //Instrução SQL que será executada
+            string SQL = "Select id, nome, sobrenome, endereco, cidade, uf, cep from Alunos Order By nome";
+            //SQLiteCommand Comando = new SQLiteCommand(SQL, ConexaoDB());
+            try
+            {
+                //Componente que recebe os dados da consulta no banco 
+                // e adpta para enviar a um data table
+                SQLiteDataAdapter da = new SQLiteDataAdapter(SQL, ConexaoDB());
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                //Ligando a grade ao data table já preparado para apresentação
+                dgwDados.DataSource = dt;
+                //Acertar o títula das colunas (deixar mais correto)
+                dgwDados.Columns[0].HeaderText = "Id";
+                dgwDados.Columns[1].HeaderText = "Nome";
+                dgwDados.Columns[2].HeaderText = "Sobrenome";
+                dgwDados.Columns[3].HeaderText = "Endereco";
+                dgwDados.Columns[3].HeaderText = "Cidade";
+                dgwDados.Columns[3].HeaderText = "UF";
+                dgwDados.Columns[3].HeaderText = "CEP";
+            }
+            catch (SQLiteException ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                //Importante fechar a conexão para não ter problemas
+                ConexaoDB().Close();
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Carregar os dados do banco na grade
+            AtualizaGrid();
+
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+             //Simplesmente limpar os campos
+            txtId.Clear();
+            txtNome.Clear();
+            txtSobrenome.Clear();
+            txtEndereco.Clear();
+            txtCidade.Clear();
+            txtCEP.Clear();
+            txtNome.Focus();
+
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            //Criar um comando (instrução SQL usando a Conexão existente)
+            using (var comando = ConexaoDB().CreateCommand())
+            {
+                //Instrução SQL para inserir dados
+                comando.CommandText = "INSERT INTO Alunos (nome,sobrenome,endereco,cidade,uf,cep ) VALUES (@nome, @sobrenome, @endereco, @cidade, @uf, @cep)";
+                //Quando utilizado parâmetros é necessário preparar a execução
+                comando.Prepare();
+                //Passar valor para cada parâmetro
+                comando.Parameters.AddWithValue("@nome", txtNome.Text);
+                comando.Parameters.AddWithValue("@sobrenome", txtSobrenome.Text);
+                comando.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+                comando.Parameters.AddWithValue("@cidade", txtCidade.Text);
+                comando.Parameters.AddWithValue("@uf", cbxUF.Text);
+                comando.Parameters.AddWithValue("@cep", txtCEP.Text);
+                try
+                {
+                    //Executar comando sem retorno de dados
+                    comando.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+
+                    //Importante fechar a conexão para não ter problemas
+                    ConexaoDB().Close();
+                    //Atualizar a grade de dados
+                    AtualizaGrid();
+                }
+            }
+
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            //Criar um comando (instrução SQL usando a Conexão existente)
+            using (var comando = ConexaoDB().CreateCommand())
+            {
+                //Instrução SQL para alterar dados
+                comando.CommandText = "Update Alunos Set nome=@nome, sobrenome=@sobrenome, endereco=@endereco, cidade=@cidade, uf=@uf, cep=@cep Where id=@id";
+                //Quando utilizado parâmetros é necessário preparar a execução
+                comando.Prepare();
+                //Passar valor para cada parâmetro
+                comando.Parameters.AddWithValue("@nome", txtNome.Text);
+                comando.Parameters.AddWithValue("@sobrenome", txtSobrenome.Text);
+                comando.Parameters.AddWithValue("@endereco", txtEndereco.Text);
+                comando.Parameters.AddWithValue("@cidade", txtCidade.Text);
+                comando.Parameters.AddWithValue("@uf", cbxUF.Text);
+                comando.Parameters.AddWithValue("@cep", txtCEP.Text);
+                comando.Parameters.AddWithValue("@id", txtId.Text);
+                try
+                {
+                    //Executar comando sem retorno de dados
+                    comando.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    //Importante fechar a conexão para não ter problemas
+                    ConexaoDB().Close();
+                    //Atualizar a grade de dados
+                    AtualizaGrid();
+                }
+            }
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            //Fazer uma pergunta via caixa de mensagem
+            var result = MessageBox.Show("Tem certeza que deseja excluir?",
+                                         "Confirmação",
+                                         MessageBoxButtons.YesNo,
+                                         MessageBoxIcon.Question);
+            //Se o usuário clicou no botão SIM...
+            if (result == DialogResult.Yes)
+            {
+                //Criar um comando (instrução SQL usando a Conexão existente)
+                using (var comando = ConexaoDB().CreateCommand())
+                {
+                    //Instrução SQL para deletar dados
+                    comando.CommandText = "Delete from Alunos Where id=@id";
+                    //Quando utilizado parâmetros é necessário preparar a execução
+                    comando.Prepare();
+                    //Passar valor para cada parâmetro
+                    comando.Parameters.AddWithValue("@id", txtId.Text);
+                    try
+                    {
+                        //Executar comando sem retorno de dados
+                        comando.ExecuteNonQuery();
+                    }
+                    catch (SQLiteException ex)
+                    {
+                        throw ex;
+                    }
+                    finally
+                    {
+                        //Importante fechar a conexão para não ter problemas
+                        ConexaoDB().Close();
+                        //Atualizar a grade de dados
+                        AtualizaGrid();
+                    }
+                }
+            }
+
+        }
+
+        private void dgwDados_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Quando navegar na grade copiar os dados para os campos
+            txtId.Text = dgwDados.CurrentRow.Cells[0].Value.ToString();
+            txtNome.Text = dgwDados.CurrentRow.Cells[1].Value.ToString();
+            txtSobrenome.Text = dgwDados.CurrentRow.Cells[2].Value.ToString();
+            txtEndereco.Text = dgwDados.CurrentRow.Cells[3].Value.ToString();
+            txtCidade.Text = dgwDados.CurrentRow.Cells[4].Value.ToString();
+            cbxUF.Text = dgwDados.CurrentRow.Cells[5].Value.ToString();
+            txtCEP.Text = dgwDados.CurrentRow.Cells[6].Value.ToString();
+
+        }
+
+        private void cbxUF_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
